252deba0defde268bd73fc7c9a66cda08c341c6d
muhammadfikri4 <muhfikriantoaji@gmail.com>
2025-11-22 21:28:46 +0700

    `fix`: test error

--- src/app/racks/racks.controller.ts
+++ src/app/racks/racks.controller.ts
@@ -51,7 +51,7 @@ export const updateRackController = async (
   );
 };
 
-export const removeRackController = async (
+export const removeRacController = async (
   req: RequestWithAccessToken,
   res: Response,
   next: NextFunction
